cc53900a435a4052e674016b23bfcda216850323
attilatoth86 <attila.toth86@gmail.com>
2015-11-22 22:23:22 +0100

    Revert "Merge branch 'master' into gh-pages"
    This reverts commit a73733b9e4717775f467d1dc55d143ef53bb9edc, reversing
    changes made to acad6e64af6b98fc9bf554ba34bdce4bf941108d.

--- 01_DataScientistsToolbox/README.md
+++
@@ -1,2 +0,0 @@
-# datasciencecoursera
-This repository is set up for course project of The Data Scientist's Toolbox
--- 02_RProgramming/ProgrammingAssignment1/pollutantmean.R
+++
@@ -1,9 +0,0 @@
-pollutantmean <- function(directory, pollutant, id=1:332) {
-    importeddata <- data.frame()
-    for (i in 1:length(id)) {
-        importeddata <- rbind(importeddata,
-                              read.csv(list.files(directory,full.names=T)[id[i]]))    
-    } 
-    selcolumn <- importeddata[pollutant]
-    mean(selcolumn[,1],na.rm=T)
-}
